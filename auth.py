35045035e5089d9e66d232952d482c2c7de0dddc
Eugene Horohorin <genixoid@gmail.com>
2019-09-27 19:05:44 +0400

    rm old files

--- auth.py
+++
@@ -1,7 +0,0 @@
-def logout():
-    return "logout"
-
-def login():
-    return "login"
-def register():
-    return "register"
--- problem.py
+++
@@ -1,8 +0,0 @@
-
-
-class Problem:
-    def __init__(self):
-        self.image = 'url'
-        self.description = 'this is important'
-        self.votes = 0
-        self.name = 'Problem'
