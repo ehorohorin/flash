df3902a5a05df8e2854231113b9fb8ce080ffda7
Eugene Horohorin <genixoid@gmail.com>
2019-09-29 02:05:45 +0400

    New version, Kotlin

---
+++ guardian/app/src/main/java/com/flash/guardian/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.flash.guardian
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.gson.Gson
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.ResultPoint
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.DefaultDecoderFactory
+import com.tbruyelle.rxpermissions2.RxPermissions
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var disposable: Disposable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        disposable = RxPermissions(this).request(Manifest.permission.CAMERA).subscribe {
+            if (it) {
+                zxing_barcode_scanner.barcodeView.decoderFactory = DefaultDecoderFactory(listOf(BarcodeFormat.QR_CODE))
+                zxing_barcode_scanner.decodeContinuous(barcodeCallback)
+            } else {
+                Toast.makeText(this, "Need camera permission", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        zxing_barcode_scanner.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        zxing_barcode_scanner.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
+    }
+
+    private val barcodeCallback = object : BarcodeCallback {
+        override fun barcodeResult(result: BarcodeResult?) {
+            result?.text?.let {
+                zxing_barcode_scanner.pause()
+                Log.d("SCANNER READ", it)
+                try {
+                    val data = Gson().fromJson(it, QrJsonData::class.java)
+                    showDialog(data.dataOk == true)
+                } catch (e: Exception) {
+                    zxing_barcode_scanner.resume()
+                    Log.d("SCANNER PARSE", e.message ?: "unknown error parse data")
+                }
+            }
+        }
+
+        override fun possibleResultPoints(resultPoints: MutableList<ResultPoint>?) {
+        }
+
+    }
+
+    private fun showDialog(isOk: Boolean) {
+        AlertDialog.Builder(this)
+            .setTitle("Просканировано")
+            .setMessage(if (isOk) "OK" else "FAIL")
+            .setCancelable(false)
+            .setPositiveButton("OK") { _, _ ->
+                zxing_barcode_scanner.resume()
+            }
+            .show()
+    }
+}
---
+++ guardian/app/src/main/java/com/flash/guardian/QrJsonData.kt
@@ -0,0 +1,5 @@
+package com.flash.guardian
+
+class QrJsonData (
+    var dataOk: Boolean? = false
+)
