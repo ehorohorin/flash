13ff816d43cd0bbc03942b1e9f5ece8161f50c38
Eugene Horohorin <genixoid@gmail.com>
2019-09-28 11:37:27 +0400

    local changes

--- guardian/app/src/main/java/com/flash/guardian/BarcodeGraphic.java
+++ guardian/app/src/main/java/com/flash/guardian/BarcodeGraphic.java
@@ -36,8 +36,6 @@ public class BarcodeGraphic extends GraphicOverlay.Graphic {
     private int mId;
 
     private static final int COLOR_CHOICES[] = {
-            Color.BLUE,
-            Color.CYAN,
             Color.GREEN
     };
 
